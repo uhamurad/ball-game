2dea71c30e23b096803978a49d96511e876920a8
NeyMain111 <Suleymanrus@mail.ru>
2021-04-28 15:20:37 +0300

    Вывод первоначальной картинки без анимации

--- ball/js/index.js
+++ ball/js/index.js
@@ -16,195 +16,9 @@ document.onkeydown = function(event){
 	
 	if (event.code === "Enter") {
 		console.log("Код активирован");
-		audio.play();
+        
 
-        init();
-        animate();
-
-        let trackMaterial = new THREE.MeshLambertMaterial({ color: '#00FF00'});
-
-        let trackGeometry = new THREE.BoxGeometry(10, 1, 100);
-
-        let tracksMeshes = Array();
-
-        let addTrack = function(x){
-
-            let trackMesh1 = new THREE.Mesh(trackGeometry, trackMaterial);
-
-            trackMesh1.position.x = x;
-            trackMesh1.position.y = -0.5;
-            trackMesh1.position.z = -50;
-            scene.add(trackMesh1);
-
-            tracksMeshes.push(trackMesh1);
-
-        }
-
-
-        addTrack(0);
-        addTrack(-11);
-        addTrack(11);
-
-        //console.log(tracksMeshes);
-
-
-        var ballMaterial = new THREE.MeshLambertMaterial({ color: '#f40a31'/*, wireframe: true*/});
-
-        var ballGeometry = new THREE.SphereGeometry(5, 30, 30);
-
-        let gameStatus = 'ready'; // 'ready', 'run', 'over'
-
-        /*let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-
-        ballMesh.position.x = 0;
-        ballMesh.position.y = 5;
-        ballMesh.position.z = -5;
-
-
-        scene.add(ballMesh);*/
-
-        let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-
-        var addBall = function () {
-
-
-
-        ballMesh.position.x = 0;
-        ballMesh.position.y = 5;
-        ballMesh.position.z = -5;
-
-
-        scene.add(ballMesh);
-
-        }
-
-        addBall();
-
-
-
-
-        var BoxMaterial = new THREE.MeshLambertMaterial({ color: '#038f8a'/*, wireframe: true*/});
-
-        var BoxGeometry = new THREE.BoxGeometry(10, 10, 10);
-
-        var BoxMeshes = Array();
-
-        let BoxMesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
-
-
-        BoxMesh.position.x = 11;
-        BoxMesh.position.y = 5;
-        BoxMesh.position.z = Math.random() * -100;
-
-
-        scene.add(BoxMesh);
-
-        let Box1Mesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
-
-
-        Box1Mesh.position.x = -11;
-        Box1Mesh.position.y = 5;
-        Box1Mesh.position.z = Math.random() * -100;
-
-
-        scene.add(Box1Mesh);
-
-        let Box2Mesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
-
-
-        Box2Mesh.position.x = 0;
-        Box2Mesh.position.y = 5;
-        Box2Mesh.position.z = Math.random() * -100;
-
-
-        scene.add(Box2Mesh);
-
-        BoxMeshes.push(BoxMesh, Box1Mesh, Box2Mesh);
-
-        //console.log(BoxMeshes);
-
-        let oneStep = function(){
-
-            /*if (ballMesh.position.z > -100){
-                ballMesh.position.z = ballMesh.position.z - 1;
-            }*/
-
-            if (tracksMeshes[0].position.z < 51){
-                //console.log(tracksMeshes[0].position.z);
-                tracksMeshes[0].position.z = tracksMeshes[0].position.z + 1; 
-                tracksMeshes[1].position.z = tracksMeshes[1].position.z + 1;
-                tracksMeshes[2].position.z = tracksMeshes[2].position.z + 1;
-            }
-
-
-            if (BoxMeshes[0].position.z < 51){
-                //console.log(BoxMeshes[0].position.z);
-                BoxMeshes[0].position.z = BoxMeshes[0].position.z + 1; 
-                BoxMeshes[1].position.z = BoxMeshes[1].position.z + 1;
-                BoxMeshes[2].position.z = BoxMeshes[2].position.z + 1;
-            }
-
-        }
-
-        let commonTimer = setInterval(function(){
-            oneStep();
-            if (gameStatus === 'over'){
-                clearInterval(commonTimer)  
-                clearInterval(leftRightTimer)
-            }
-        }, 50);
-
-
-        let checkBallHasRightPosition = function(){
-
-            if (ballMesh.position.x < -11 || ballMesh.position.x > 11) {
-                alert('DEATH');
-                gameStatus = 'over';
-            }
-
-
-        }
-
-        let leftRightTimer;
-        document.onkeydown = function(event){
-
-            //console.log(event);	
-
-            if (gameStatus === 'run'){
-
-                if (event.code === 'ArrowLeft'){
-
-                    let limitX = ballMesh.position.x - 11;
-                    leftRightTimer = setInterval(function(){
-
-                        ballMesh.position.x = ballMesh.position.x - 1;
-                        checkBallHasRightPosition();
-
-                        if (ballMesh.position.x <= limitX ){
-                            clearInterval(leftRightTimer);
-                        }
-                    }, 10);
-
-                } else if (event.code === "ArrowRight") {
-
-                    var limitX = ballMesh.position.x + 11;
-                    leftRightTimer = setInterval(function(){
-
-                        ballMesh.position.x = ballMesh.position.x + 1;
-                        checkBallHasRightPosition();
-
-                        if (ballMesh.position.x >= limitX ){
-                            clearInterval(leftRightTimer);
-                        }
-
-
-                    }, 10);
-                }
-
-            }
-
-        }
-
+        audio.play();
 	}
 
 /*let oneStep1 = function(){
@@ -225,9 +39,200 @@ sleep(100);
 
 
 
+// Инициализация объектов сцены и т .п.
+init();
+
+
+//animate();
+
+let trackMaterial = new THREE.MeshLambertMaterial({ color: '#00FF00'});
+
+let trackGeometry = new THREE.BoxGeometry(10, 1, 100);
+
+let tracksMeshes = Array();
+
+let addTrack = function(x){
+
+    let trackMesh1 = new THREE.Mesh(trackGeometry, trackMaterial);
+
+    trackMesh1.position.x = x;
+    trackMesh1.position.y = -0.5;
+    trackMesh1.position.z = -50;
+    scene.add(trackMesh1);
+
+    tracksMeshes.push(trackMesh1);
+
+}
+
+
+addTrack(0);
+addTrack(-11);
+addTrack(11);
+
+//console.log(tracksMeshes);
+
+
+var ballMaterial = new THREE.MeshLambertMaterial({ color: '#f40a31'/*, wireframe: true*/});
+
+var ballGeometry = new THREE.SphereGeometry(5, 30, 30);
+
+let gameStatus = 'ready'; // 'ready', 'run', 'over'
+
+/*let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+
+ballMesh.position.x = 0;
+ballMesh.position.y = 5;
+ballMesh.position.z = -5;
+
+
+scene.add(ballMesh);*/
+
+let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+
+var addBall = function () {
 
 
 
+ballMesh.position.x = 0;
+ballMesh.position.y = 5;
+ballMesh.position.z = -5;
+
+
+scene.add(ballMesh);
+
+}
+
+addBall();
+
+
+
+var BoxMaterial = new THREE.MeshLambertMaterial({ color: '#038f8a'/*, wireframe: true*/});
+
+var BoxGeometry = new THREE.BoxGeometry(10, 10, 10);
+
+var BoxMeshes = Array();
+
+let BoxMesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
+
+
+BoxMesh.position.x = 11;
+BoxMesh.position.y = 5;
+BoxMesh.position.z = Math.random() * -100;
+
+
+scene.add(BoxMesh);
+
+let Box1Mesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
+
+
+Box1Mesh.position.x = -11;
+Box1Mesh.position.y = 5;
+Box1Mesh.position.z = Math.random() * -100;
+
+
+scene.add(Box1Mesh);
+
+let Box2Mesh = new THREE.Mesh(BoxGeometry, BoxMaterial);
+
+
+Box2Mesh.position.x = 0;
+Box2Mesh.position.y = 5;
+Box2Mesh.position.z = Math.random() * -100;
+
+
+scene.add(Box2Mesh);
+
+BoxMeshes.push(BoxMesh, Box1Mesh, Box2Mesh);
+
+//console.log(BoxMeshes);
+
+let oneStep = function(){
+
+    /*if (ballMesh.position.z > -100){
+        ballMesh.position.z = ballMesh.position.z - 1;
+    }*/
+
+    if (tracksMeshes[0].position.z < 51){
+        //console.log(tracksMeshes[0].position.z);
+        tracksMeshes[0].position.z = tracksMeshes[0].position.z + 1; 
+        tracksMeshes[1].position.z = tracksMeshes[1].position.z + 1;
+        tracksMeshes[2].position.z = tracksMeshes[2].position.z + 1;
+    }
+
+
+    if (BoxMeshes[0].position.z < 51){
+        //console.log(BoxMeshes[0].position.z);
+        BoxMeshes[0].position.z = BoxMeshes[0].position.z + 1; 
+        BoxMeshes[1].position.z = BoxMeshes[1].position.z + 1;
+        BoxMeshes[2].position.z = BoxMeshes[2].position.z + 1;
+    }
+
+}
+
+let commonTimer = setInterval(function(){
+    oneStep();
+    if (gameStatus === 'over'){
+        clearInterval(commonTimer)  
+        clearInterval(leftRightTimer)
+    }
+}, 50);
+
+
+let checkBallHasRightPosition = function(){
+
+    if (ballMesh.position.x < -11 || ballMesh.position.x > 11) {
+        alert('DEATH');
+        gameStatus = 'over';
+    }
+
+
+}
+
+let leftRightTimer;
+document.onkeydown = function(event){
+
+    //console.log(event);	
+
+    if (gameStatus === 'run'){
+
+        if (event.code === 'ArrowLeft'){
+
+            let limitX = ballMesh.position.x - 11;
+            leftRightTimer = setInterval(function(){
+
+                ballMesh.position.x = ballMesh.position.x - 1;
+                checkBallHasRightPosition();
+
+                if (ballMesh.position.x <= limitX ){
+                    clearInterval(leftRightTimer);
+                }
+            }, 10);
+
+        } else if (event.code === "ArrowRight") {
+
+            var limitX = ballMesh.position.x + 11;
+            leftRightTimer = setInterval(function(){
+
+                ballMesh.position.x = ballMesh.position.x + 1;
+                checkBallHasRightPosition();
+
+                if (ballMesh.position.x >= limitX ){
+                    clearInterval(leftRightTimer);
+                }
+
+
+            }, 10);
+        }
+
+    }
+
+}
+
+
+
+// Первоначальный рендер картинки
+
+render();
 
 
 
